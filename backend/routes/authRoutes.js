503f325e6afa823b3c40118d0992202f25365b45
dhruuujs <dhrubajiban78901@gmail.com>
2025-09-14 15:34:57 +0530

    Deleted messages logic,lol

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,10 @@
 //Buble
 import express from 'express';
-//import {storeMsgs,deleteMsg} from '../controllers/'
 import {login,signup} from '../controllers/authControllers.js'
 import {authenticate} from '../middleware/auth.js'
 
 const router = express.Router();
 
-router.post("/chat",storeMsgs);
 router.post("/login",login);
 router.post("/signup",signup);
 
