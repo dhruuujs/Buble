fed22e3e86e1b6eed0f3f6959e739c4f318f6c38
dhruuujs <dhrubajiban78901@gmail.com>
2025-09-14 15:27:04 +0530

    Updated app.js

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,39 @@
-import express from 'express';
+import express,{urlencoded} from 'express';
+import dotenv from 'dotenv';
+import helmet from 'helmet';
 import cors from 'cors';
-import '../backend/server.js';
+import login from '../backend/routes/authRoutes.js'
+import signup from '../backend/routes/authRoutes.js'
+import auth from '../backend/routes/authRoutes.js'
 
-const app= express();
-app.use(cors());
+const app=express();
+dotenv.config();
 
-app.get('/',(req,res)=>{
-    res.send("Connected to the server");
-});
+//Configs
+app.use(cors({
+    orgin:"http://localhost:5173",
+    optionsSuccessStatus:200
+    }))
+app.use(helmet());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
-module.exports=app;
+//Routers
+try{
+app.use("/account",login);
+app.use("/account",signup);
+app.use("/account",auth);
+
+
+
+
+}catch(err){
+  console.log(err);
+}
+
+app.get("/",(req,res)=>{
+res.status(200).send(`Server up and listening on port 5000, nigga`)
+})
+
+
+export default app;
