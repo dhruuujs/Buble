6a1a3233fa75af94c057f77327adca895aa6d5a0
dhruuujs <dhrubajiban78901@gmail.com>
2025-09-16 19:16:21 +0530

    Fixed jwt token expire bug

--- backend/utils/jwt.js
+++ backend/utils/jwt.js
@@ -3,19 +3,22 @@ import dotenv from 'dotenv';
 import jwtpkg from 'jsonwebtoken';
 dotenv.config();
 
-
 const {sign,verify} = jwtpkg;
 const { JWT_SECRET_KEY, JWT_EXPIRES_IN } = process.env;
 
 
-export const generateToken = (userId,username)=>{
+export const generateToken = (userId,user)=>{
     return sign(
-        {userId,username},
-          JWT_SECRET_KEY || "usPnV2sL0x5bsFWsbmoqMG",
-        {expiresIn: JWT_EXPIRES_IN || 3600});
+        {userId,user},
+        JWT_SECRET_KEY || "usPnV2sL0x5bsFWsbmoqMG",
+        {expiresIn: JWT_EXPIRES_IN}
+    );
 }
 
-
-export const verifyToken=(token)=>{
-    return verify(token, JWT_SECRET_KEY || "usPnV2sL0x5bsFWsbmoqMG");
+export const verifyToken = (token)=>{
+  try{
+return verify(token, JWT_SECRET_KEY || "usPnV2sL0x5bsFWsbmoqMG");
+  }catch(err){
+    return err;
+  }
 }
