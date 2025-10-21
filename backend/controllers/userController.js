cd68f3fcd1648fc45f1a045064fc05ba52e12b1a
dhruuujs <dhrubajiban78901@gmail.com>
2025-10-21 23:01:16 +0530

    Minor Update

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -3,15 +3,13 @@ import Users from '../models/usermodel.js'
 export const fetchUser = async (req,res)=>{
 try{
     const {username}= await req.body;
-if(!username){
-return res.status(400);    
-}
-
-
+    if(!username){
+    return res.status(400).json({error:"Empty fields is not allowed"});    
+    }
+    const friendList = Users.findUserByUsername(username);
+    return res.status(200).json({friends:friendList})
 }catch(err){
-
+    return new Error(err); 
 }
-
-
 
 } 
