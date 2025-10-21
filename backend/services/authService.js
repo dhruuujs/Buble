01682a714f47f7fdf5feffd7fd834eae3ee82fad
dhruuujs <dhrubajiban78901@gmail.com>
2025-10-21 23:03:11 +0530

    Minor Update

--- backend/services/authService.js
+++ backend/services/authService.js
@@ -5,69 +5,45 @@ import {generateToken} from '../utils/jwt.js'
 
 class AuthService{
 //Signup Function
-    static async signupUser(username,password,secondCred){
-        
-        
-        if(!username){
-            throw new Error("Username required");
-        }
-
-        if(!password){
-            throw new Error("Password required");        
-        }
-
-        if(!secondCred){
-            throw new Error("Email or phone number required");
-               
-        }
-
-
-//Password to hashed
-        const hashedPassword = await bcrypt.hash(password,12)
-
-//Create users    
-    const user = await Users.createAccount(
-        username,
-        hashedPassword,
-        secondCred);
-
+    static async signupUser(username,password,secondCred,){
+        try{ 
+    const hashedPassword = await bcrypt.hash(password,12)
+    const user = await Users.createAccount(username,hashedPassword,secondCred);
+    console.log("User ",user.username+" created.")    
     const token = generateToken(user.user_id,user.username)
-    //console.log(user)
-    return {user,token}
-
+    return {user:user.username,token}
+        }catch(err){
+            return {error:err.message}
+        }
 }
-
 
 
 //Login Function
 static async loginUser(username,password){
-
    try{ 
-    
     if(!username){
         throw new Error("Invalid username");
     }
-
     if(!password){
         throw new Error("Invalid password");
     }
-
     const user = await Users.findUserByUsername(username);
+    
     if(!user){
         throw new Error("Username doesn't exist");
     }
-    
+    console.log("Password Boolean",user.password_hash);
     const isMatch = await bcrypt.compare(password,user.password_hash)
     if(!isMatch){
         throw new Error("Password doesn't match");
     }
-    const token = generateToken(user.user_id,user.username);  
+    const token = generateToken(user.user_id,user);  
     const {password_hash:_,...userWithoutPassword} = user;
     
     return {user:userWithoutPassword,token}
 
 }catch(err){
-    return err.message
+    throw new Error(err.message);
 }
 
 }
