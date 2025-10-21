bb79b3108428c5039ee474c665a7387c99075359
dhruuujs <dhrubajiban78901@gmail.com>
2025-10-21 22:59:54 +0530

    Minor Update

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,13 @@
 import http from 'http';
 import app from '../backend/app.js';
 import {Server} from 'socket.io';
+import setupSocketIO from '../backend/socket/index.js'
 
 const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
-const io = new Server(server);
+const io = new Server(server,{cors:{
+    origin:'*',
+    method:['GET','POST']}});
 
-
-io.on('connection',(socket)=>{
-  console.log("A client connected")
-})
+    setupSocketIO(io);
 server.listen(5000,()=>console.log(`Server Banana running on port: ${PORT}`));
