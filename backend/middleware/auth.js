127e820f1df5a0902e88c59a14f8e8e65866632c
dhruuujs <dhrubajiban78901@gmail.com>
2025-10-21 23:03:27 +0530

    Minor Update

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,28 +3,36 @@ import Users from '../models/usermodel.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
-export const authenticate = async(req,res)=>{
+export const authenticate = async(req,res,next)=>{
     if(req.method=='OPTIONS'){
         return res.sendStatus(204);
     }
-
 try{
 const authHeader =req.headers.authorization;
-const token = authHeader?.split('')[1];
+const token = authHeader?.split(' ')[1];
 
 if(!token){
     return res.status(400).json({error:`Authorization token required or token not found error:${token}`})
 }
-const decoded = verifyToken(token);
-const userToken = await Users.findById(decoded.userId)
 
+const decoded = verifyToken(token);
+//console.log("Decoded token from auth.js", decoded);
+
+if (!decoded || decoded.name === 'TokenExpiredError') {
+    return res.status(401).json({
+        error: decoded.name === 'TokenExpiredError' ? "Token Expired" : "Unable to decode user data!"
+    });
+}
+const userToken = await Users.findUserById(decoded.userId)
 if(!userToken){
     return res.status(400).json({
-        error: 'Invalid token user not found'
+        error: 'Invalid token, user not found'
     })
 }
 req.user=userToken;
+next();
     }catch(err){
-        console.log("Error at auth.js, Error: ",err)
+        res.status(500).json({error:err.message});
+        console.log("Error at auth.js, Error: ",err.message)
     }
 }
